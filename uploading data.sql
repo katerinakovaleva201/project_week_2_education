61253c45b84da8fa6b68cea6b7bc0d1532e240c6
Bulduk Eker <bulduk.eker@gmail.com>
2023-01-27 16:16:03 +0100

    adding the SQL script and index calculation

---
+++ uploading data.sql
@@ -0,0 +1,13 @@
+USE Education;
+
+INSERT INTO Countries (country_code, country_name)
+VALUES ("EST", "Estonia"),
+("BGR", "Bulgaria"),
+("HUN", "Hungary"),
+("LTU", "Lithuania"),
+("LVA", "Latvia"),
+("MDA", "Moldova"),
+("POL", "Poland"),
+("ROU", "Romania"),
+("RUS", "Russia"),
+("UKR", "Ukraine");
